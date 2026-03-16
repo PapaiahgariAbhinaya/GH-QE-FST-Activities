ca745a0116f4c98d77f01c158629832cf4945dde
PapaiahgariAbhinaya <abhinayapapaiahgari@gmail.com>
2026-03-16 16:39:36 +0530

    Add files via upload

---
+++ TestNG/Activities/java/TestNG/Act4_2_TestNG.java
@@ -0,0 +1,10 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class Act4_2_TestNG {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
